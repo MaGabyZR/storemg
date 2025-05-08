54bb73826d0e99f1b8c10a5cb89368526969c0b8
Gabriela Rubio <gabyrubioa@gmail.com>
2025-05-08 10:02:42 -0600

    Initial Commit for storemg

---
+++ src/main/java/com/magabyzr/storemg/StoremgApplication.java
@@ -0,0 +1,13 @@
+package com.magabyzr.storemg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoremgApplication.class, args);
+    }
+
+}
