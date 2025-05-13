560f6cbaadb06b3e8d1c0fe59c9537a695f843a3
Gabriela Rubio <gabyrubioa@gmail.com>
2025-05-13 11:36:14 -0600

    setter injection.

--- src/main/java/com/magabyzr/storemg/OrderService.java
+++ src/main/java/com/magabyzr/storemg/OrderService.java
@@ -1,9 +1,17 @@
 package com.magabyzr.storemg;
 
 public class OrderService {
-    public void placeOrder(){
-        var paymentService = new StripePaymentService();
-        paymentService.processPayment(10);
+    private PaymentService paymentService;                                  //setter below.
 
+//    public OrderService(PaymentService paymentService){
+//       this.paymentService = paymentService;
+//    }
+
+    public void placeOrder(){
+        paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {          //setter.
+        this.paymentService = paymentService;
     }
 }
--- src/main/java/com/magabyzr/storemg/StoremgApplication.java
+++ src/main/java/com/magabyzr/storemg/StoremgApplication.java
@@ -2,12 +2,17 @@ package com.magabyzr.storemg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class StoremgApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoremgApplication.class, args);
+        //SpringApplication.run(StoremgApplication.class, args);
+        //var orderService = new OrderService(new StripePaymentService());
+        var orderService = new OrderService();
+        orderService.setPaymentService(new PayPalPaymentService());                     //to use the setter from orderService.
+        orderService.placeOrder();
     }
 
 }
